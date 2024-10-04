84d380950b5faf2f38097162c0de15e6321f6537
Jefffreypng <1618070@senati.pe>
2024-10-04 17:42:06 -0500

    py

---
+++ secuenciales/02.py
@@ -0,0 +1,11 @@
+m=float(input("Metros: "))
+
+cm=m*100.0
+pulg=m*100*2.54
+pie=m*100*2.54*12
+yard=m*100*2.54*12*3
+
+print(f"Centímetros: {cm:.2f}")
+print(f"Pulgadas: {pulg:.2f}")
+print(f"Pies: {pie:.2f}")
+print(f"Yardas: {yard:.2f}")
