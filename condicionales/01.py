69888abe0d6a076e44372fe3c911763e0dd6c98e
Jefffrey-jpg <1618070@senati.pe>
2024-10-05 09:52:19 -0500

    py

--- condicionales/01.py
+++ condicionales/01.py
@@ -1,6 +1,7 @@
-cantProd=int(input("Cantidad del producto: "))
+import os
+os.system("cls")
 
-def calcularPrecio(cantProd)
+def calcularPrecio(cantProd):
     if 1<= cantProd <=25 :
         precioUnidad=27
     elif 26<= cantProd <=50 :
@@ -8,4 +9,16 @@ def calcularPrecio(cantProd)
     elif 51<= cantProd :
         precioUnidad=23
     else :
-        return "Ingrese otra cantidad"
+        return "Otro monto"
+
+    importe = cantProd * precioUnidad
+    descuento = 0.15 * importe if cantProd >50 else 0.5 * importe
+    total = importe - descuento
+    return round(importe, 2), round(descuento, 2), round(total, 2)
+
+cantProd=int(input("Ingrese la cantidad del producto: "))
+importe, descuento, total = calcularPrecio(cantProd)
+
+print(f"Importe: S/. {importe}")
+print(f"Descuento: S/. {descuento}")
+print(f"Total: S/. {total}")
