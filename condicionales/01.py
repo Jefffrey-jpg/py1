53f647f22f062276543e120ba9702065d7a8e1c5
Jefffrey-jpg <1618070@senati.pe>
2024-10-05 09:24:31 -0500

    py

--- condicionales/01.py
+++ condicionales/01.py
@@ -1,5 +1,11 @@
 cantProd=int(input("Cantidad del producto: "))
 
-if cantProd >=1 <25 :
-    print("hola")
-
+def calcularPrecio(cantProd)
+    if 1<= cantProd <=25 :
+        precioUnidad=27
+    elif 26<= cantProd <=50 :
+        precioUnidad=25
+    elif 51<= cantProd :
+        precioUnidad=23
+    else :
+        return "Ingrese otra cantidad"
