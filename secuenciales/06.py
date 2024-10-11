fc0e6c27326979c28bcef3e5193bb1f222f8a529
Jefffrey-jpg <1618070@senati.pe>
2024-10-11 16:55:13 -0500

    py5

--- secuenciales/06.py
+++ secuenciales/06.py
@@ -1,4 +1,4 @@
-import os
+import os, math
 os.system("cls")
 
 print("Calcular área y volumen del cilindro")
@@ -6,10 +6,10 @@ print("Calcular área y volumen del cilindro")
 h = int(input("Altura: "))
 r = int(input("Radio: "))
 
-areaLateral = 2 * 3.14159264 * r * h
-areaBases = 2 * 3.14159264 * r * r
+areaLateral = 2 * math.pi * r * h
+areaBases = 2 * math.pi * r ** 2
 areaTotal = areaBases + areaLateral
-vol = 3.14159264 * r * r * h
+vol = math.pi * h * r ** 2
 
 print(f"El área del cilindro es: {areaTotal:.2f} m^2")
 print(f"El volumen del cilindro es: {vol:.2f} m^3")
--- secuenciales/08.py
+++ secuenciales/08.py
@@ -1,11 +1,11 @@
-import os
+import os, math
 os.system("cls")
 
 r = int(input("Ingrese el radio del cilindro: "))
 h = int(input("Ingrese la altura del cilindro: "))
 
-areaBase = 3.14159264 * r * r
-areaLateral = 2 * 3.14159264 * r * h
+areaBase = math.pi * r ** 2
+areaLateral = 2 * math.pi * r * h
 areaTotal= 2 * areaBase + areaLateral
 
 print(f"Area de la base del cilindro: {areaBase:.2f} u^2")
