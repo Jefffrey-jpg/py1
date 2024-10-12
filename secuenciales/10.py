701c9294bfdb74646c76aefca55b54a75ec619df
Jefffrey-jpg <1618070@senati.pe>
2024-10-11 23:33:52 -0500

    a

--- secuenciales/10.py
+++ secuenciales/10.py
@@ -1,10 +1,10 @@
 import os
 os.system("cls")
 
-numero = input("Ingrese un numero de 4 cifras: ")
+numero = input("Número de 4 cifras: ")
 
-if len(numero) == 4 and numero.isdigit():
-    numeroInvertido = numero[::-1]
-    print(f"Número al revés: {numeroInvertido}")
-else:
-    print("El numero no tiene 4 cifras.")
+numeroReves = numero[::-1]
+
+resultado = (len(numero) == 4) * f"Número al revés: {numeroReves}" + (len(numero) != 4) * "El número no tiene 4 cifras."
+
+print(resultado)
