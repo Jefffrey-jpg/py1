dd329e9cee4649bdb9a14614442310a1fc30d2e7
Jefffrey-jpg <1618070@senati.pe>
2024-10-13 13:24:03 -0500

    c

--- condicionales/09.py
+++ condicionales/09.py
@@ -1,3 +1,35 @@
 import os
 os.system("cls")
 
+producto = int(input("Ingrese el codigo del producto (101, 102, 103, 104): "))
+unidades = int(input("Ingrese las unidades compradas: "))
+
+if producto == 101 :
+    precio = 17
+elif producto == 102 :
+    precio = 25
+elif producto == 103 :
+    precio = 16
+elif producto == 104 :
+    precio = 27
+else :
+    print("Código de producto no válido")
+
+if 1 <= unidades <= 10 :
+    descuento = 0.05
+elif 11 <= unidades <= 20 :
+    descuento = 0.08
+elif 21 <= unidades <= 30 :
+    descuento = 0.10
+elif 31 <= unidades :
+    descuento = 0.13
+else :
+    print("Descuento no aplicable")
+
+importe = producto * unidades
+montoDescuento = importe * descuento
+precioFinal = importe - montoDescuento
+
+print(f"Importe total: S/. {importe}")
+print(f"Monto descontado: S/. {montoDescuento}")
+print(f"Precio final: S/. {precioFinal}")
